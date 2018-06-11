be37ebf1b7e3cfe600ec1bc5964f87d060b50e47
Atila Budai <atib1980@gmail.com>
2018-06-11 03:16:44 +0200

    optimization using hash and move

--- 2_medium_difficulty_level/39_combination_sum.cpp
+++ 2_medium_difficulty_level/39_combination_sum.cpp
@@ -36,6 +36,7 @@ A solution set is:
 #include <algorithm>
 #include <iostream>
 #include <queue>
+#include <string>
 #include <tuple>
 #include <unordered_set>
 #include <vector>
@@ -45,6 +46,7 @@ using std::get;
 using std::ostream;
 using std::queue;
 using std::sort;
+using std::string;
 using std::tuple;
 using std::unordered_set;
 using std::vector;
@@ -66,8 +68,25 @@ ostream& operator<<(ostream& os, const vector<T>& container) {
 }
 
 class Solution {
+	
+  static void generate_hash_index(const vector<int>& seq, string& hash_index) {
+    hash_index.clear();
+
+    for (int n : seq) {
+      while (n) {
+        hash_index.push_back(n % 10 + '0');
+        n /= 10;
+      }
+
+      hash_index.push_back(',');
+    }
+
+    hash_index.pop_back();
+  }
+
  public:
-  vector<vector<int>> combinationSum(vector<int> candidates, const int target) {
+  vector<vector<int>> combinationSum(vector<int>& candidates,
+                                     const int target) {
     size_t candidates_size{candidates.size()};
 
     for (size_t i{}; i < candidates_size; i++) {
@@ -83,6 +102,9 @@ class Solution {
     queue<tuple<size_t, int, vector<int>>> q{{make_tuple(0, 0, vector<int>{})}};
 
     unordered_set<int> already_visited_first_numbers{};
+    unordered_set<string> already_visited_sequences{};
+    string hash_index{};
+    hash_index.reserve(1024);
 
     vector<vector<int>> unique_combinations{};
 
@@ -95,38 +117,40 @@ class Solution {
       for (size_t i{current_pos}; i < candidates_size; i++) {
         if (target == current_sum + candidates[i]) {
           current_seq.emplace_back(candidates[i]);
-          bool is_found{};
-
-          for (const vector<int>& combination : unique_combinations) {
-            if (combination == current_seq) {
-              is_found = true;
-              break;
-            }
-          }
-
-          if (!is_found)
-            unique_combinations.emplace_back(move(current_seq));
+          generate_hash_index(current_seq, hash_index);
+          if (already_visited_sequences.count(hash_index))
+            break;
+          already_visited_sequences.insert(hash_index);
+          unique_combinations.push_back(move(current_seq));
           break;
 
         } else if (current_sum + candidates[i] > target)
           break;
 
         else {
-          if (current_seq.empty() &&
-              already_visited_first_numbers.find(candidates[i]) !=
-                  end(already_visited_first_numbers))
-            continue;
-          already_visited_first_numbers.insert(candidates[i]);
           if (current_seq.empty()) {
-            if (i + 1 < candidates_size)
-              q.emplace(
-                  make_tuple(i + 1, candidates[i], vector<int>{candidates[i]}));
+            if (already_visited_first_numbers.count(candidates[i]))
+              continue;
+            already_visited_first_numbers.insert(candidates[i]);
+            q.emplace(
+                make_tuple(i + 1, candidates[i], vector<int>{candidates[i]}));
           } else {
             current_seq.emplace_back(candidates[i]);
-            if (i + 1 < candidates_size)
+            generate_hash_index(current_seq, hash_index);
+            if (already_visited_sequences.count(hash_index)) {
+              current_seq.pop_back();
+              continue;
+            }
+            already_visited_sequences.insert(hash_index);
+            if (i < candidates_size - 1) {
               q.emplace(
                   make_tuple(i + 1, current_sum + candidates[i], current_seq));
-            current_seq.pop_back();
+              current_seq.pop_back();
+            } else {
+              q.emplace(make_tuple(i + 1, current_sum + candidates[i],
+                                   move(current_seq)));
+              break;
+            }
           }
         }
       }
@@ -138,26 +162,27 @@ class Solution {
 
 int main() {
   Solution s{};
-  vector<vector<int>> result_set{s.combinationSum(vector<int>{2, 3, 6, 7}, 7)};
+  vector<int> input{2, 3, 6, 7};
+  vector<vector<int>> result_set{s.combinationSum(input, 7)};
 
   cout << '\n';
-  for (const auto& v : result_set)
+  for (const vector<int>& v : result_set)
     cout << v << '\n';
 
-  result_set = s.combinationSum(vector<int>{2, 3, 5}, 8);
+  input.assign({2, 3, 5});
+  result_set = s.combinationSum(input, 8);
 
   cout << '\n';
-  for (const auto& v : result_set)
+  for (const vector<int>& v : result_set)
     cout << v << '\n';
 
-  result_set = s.combinationSum(
-      vector<int>{92,  71,  89, 74,  102, 91,  70,  119, 86, 116,
-                  114, 106, 80, 81,  115, 99,  117, 93,  76, 77,
-                  111, 110, 75, 104, 95,  112, 94,  73},
-      310);
+  input.assign({92,  71,  89, 74,  102, 91,  70,  119, 86, 116,
+                114, 106, 80, 81,  115, 99,  117, 93,  76, 77,
+                111, 110, 75, 104, 95,  112, 94,  73});
+  result_set = s.combinationSum(input, 310);
 
   cout << '\n';
-  for (const auto& v : result_set)
+  for (const vector<int>& v : result_set)
     cout << v << '\n';
 
   return 0;
