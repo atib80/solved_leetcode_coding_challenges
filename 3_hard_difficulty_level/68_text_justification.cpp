0953d8f34013583b8b8a500496863f653b5238ff
Atila Budai <atib1980@gmail.com>
2018-07-02 07:21:30 +0200

    init commit of first version

---
+++ 3_hard_difficulty_level/68_text_justification.cpp
@@ -0,0 +1,185 @@
+/*
+68. Leetcode coding challenge: Text justification
+
+Given an array of words and a width maxWidth, format the text such that each
+line has exactly maxWidth characters and is fully (left and right) justified.
+
+You should pack your words in a greedy approach; that is, pack as many words as
+you can in each line. Pad extra spaces ' ' when necessary so that each line has
+exactly maxWidth characters.
+
+Extra spaces between words should be distributed as evenly as possible. If the
+number of spaces on a line do not divide evenly between words, the empty slots
+on the left will be assigned more spaces than the slots on the right.
+
+For the last line of text, it should be left justified and no extra space is
+inserted between words.
+
+Note:
+
+    A word is defined as a character sequence consisting of non-space characters
+only. Each word's length is guaranteed to be greater than 0 and not exceed
+maxWidth. The input array words contains at least one word.
+
+Example 1:
+
+Input:
+words = ["This", "is", "an", "example", "of", "text", "justification."]
+maxWidth = 16
+Output:
+[
+   "This    is    an",
+   "example  of text",
+   "justification.  "
+]
+
+Example 2:
+
+Input:
+words = ["What","must","be","acknowledgment","shall","be"]
+maxWidth = 16
+Output:
+[
+  "What   must   be",
+  "acknowledgment  ",
+  "shall be        "
+]
+Explanation: Note that the last line is "shall be    " instead of "shall be",
+             because the last line must be left-justified instead of
+fully-justified. Note that the second line is also left-justified becase it
+contains only one word.
+
+Example 3:
+
+Input:
+words = ["Science","is","what","we","understand","well","enough","to","explain",
+         "to","a","computer.","Art","is","everything","else","we","do"]
+maxWidth = 20
+Output:
+[
+  "Science  is  what we",
+  "understand      well",
+  "enough to explain to",
+  "a  computer.  Art is",
+  "everything  else  we",
+  "do                  "
+]
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+static int sres = []() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  return 0;
+}();
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& data) {
+  if (data.empty()) {
+    os << "data is empty\n";
+    return os;
+  }
+  os << "\n[";
+  for (size_t i{}; i < data.size() - 1; i++)
+    os << data[i] << ',';
+  os << data.back() << "]\n";
+  return os;
+}
+
+class Solution {
+ public:
+  vector<string> fullJustify(vector<string>& words, const size_t maxWidth) {
+    const size_t cnt_words{words.size()};
+    size_t cnt_processed_words{};
+
+    vector<string> formatted_lines{};
+    formatted_lines.reserve(words.size());
+
+    // size_t index{};
+
+    while (true) {
+      size_t cnt_of_words_for_line{1};
+      size_t cnt_of_letters{words[cnt_processed_words].length()};
+      vector<size_t> word_indices{cnt_processed_words};
+      for (size_t i{cnt_processed_words + 1}; i < cnt_words; i++) {
+        if (cnt_of_letters + cnt_of_words_for_line + words[i].length() >
+            maxWidth)
+          break;
+        cnt_of_words_for_line++;
+        cnt_of_letters += words[i].length();
+        word_indices.emplace_back(i);
+      }
+
+      cnt_processed_words += cnt_of_words_for_line;
+      const bool last_line{cnt_processed_words == cnt_words};
+      const size_t cnt_of_space_locations{cnt_of_words_for_line - 1};
+      const size_t cnt_of_all_spaces{maxWidth - cnt_of_letters};
+      const size_t cnt_of_word_spaces{
+          cnt_of_space_locations ? cnt_of_all_spaces / cnt_of_space_locations
+                                 : cnt_of_all_spaces};
+      size_t cnt_of_excess_spaces{
+          cnt_of_space_locations ? cnt_of_all_spaces % cnt_of_space_locations
+                                 : 0};
+
+      ostringstream oss{};
+      const string spaces_str(cnt_of_word_spaces, ' ');
+
+      for (size_t i{}; i < word_indices.size() - 1; i++) {
+        oss << words[word_indices[i]];
+        if (!last_line) {
+          oss << spaces_str;
+          if (cnt_of_excess_spaces) {
+            cnt_of_excess_spaces--;
+            oss << ' ';
+          }
+        } else
+          oss << ' ';
+      }
+
+      oss << words[word_indices.back()];
+      if (last_line || 1 == cnt_of_words_for_line)
+        oss << spaces_str;
+
+      formatted_lines.push_back(move(oss.str()));
+      // words[index] = move(oss.str());
+      // index++;
+
+      if (last_line) {
+        return formatted_lines;
+        // words.erase(begin(words) + index, end(words));
+        // return move(words);
+      }
+    }
+  }
+};
+
+int main() {
+  Solution s{};
+
+  vector<string> input{"This",          "is", "an", "example", "of", "text",
+                       "justification."};
+  vector<string> output{s.fullJustify(input, 16)};
+
+  cout << " s.fullJustify(input, 16) -> " << output << '\n';
+
+  input.assign({"What", "must", "be", "acknowledgment", "shall", "be"});
+  output = s.fullJustify(input, 16);
+
+  cout << " s.fullJustify(input, 16) -> " << output << '\n';
+
+  input.assign({"Science", "is", "what", "we", "understand", "well", "enough",
+                "to", "explain", "to", "a", "computer.", "Art", "is",
+                "everything", "else", "we", "do"});
+  output = s.fullJustify(input, 20);
+
+  cout << " s.fullJustify(input, 20) -> " << output << '\n';
+
+  return 0;
+}
