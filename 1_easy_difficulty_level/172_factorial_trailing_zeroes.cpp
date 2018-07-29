2e39f6ea44411ef608d232685c50c7ba2a16537e
Atila Budai <atib1980@gmail.com>
2018-07-29 23:24:09 +0200

    simple iterative solution (accepted)

--- 1_easy_difficulty_level/172_factorial_trailing_zeroes.cpp
+++ 1_easy_difficulty_level/172_factorial_trailing_zeroes.cpp
@@ -19,54 +19,26 @@ Note: Your solution should be in logarithmic time complexity.
 */
 
 #include <cstdint>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 using namespace std;
 
-static double fibonacci_numbers[21]{};
-
-constexpr void generate_fibonacci_numbers() {
-  fibonacci_numbers[0] = fibonacci_numbers[1] = 1;
-
-  for (size_t i{2}; i < 21; i++) {
-    double result = i;
-
-    for (uint64_t j{i - 1}; j > 1; j--)
-      result *= j;
-    fibonacci_numbers[i] = result;
-  }
-}
-
 class Solution {
-  static constexpr double epsilon{1E-15};
-  static double calculate_fibonacci(const size_t n) {
-    double result = n;
-    for (size_t i{n - 1}; i > 1; i--)
-      result *= i;
-    return result;
-  }
-
  public:
-  int trailingZeroes(const int n) {
-    if (n < 5)
-      return 0;
-    double fibonacci{n < 21 ? fibonacci_numbers[n] : calculate_fibonacci(n)};
+  int trailingZeroes(int n) {
     int zero_count{};
-    while (fibonacci > 10) {
-      double factor{fibonacci / 10};
-      if (factor - trunc(factor) < epsilon) {
-        zero_count++;
-        fibonacci = factor;
-      } else
-        break;
+    while (n >= 5) {
+      zero_count += n / 5;
+      n /= 5;
     }
     return zero_count;
   }
 };
 
 int main() {
-  generate_fibonacci_numbers();
   Solution s{};
 
   cout << "s.trailingZeroes(3) -> " << s.trailingZeroes(3)
@@ -82,7 +54,9 @@ int main() {
   cout << "s.trailingZeroes(9) -> " << s.trailingZeroes(9)
        << '\n';  // expected output: 1
   cout << "s.trailingZeroes(10) -> " << s.trailingZeroes(10)
-       << '\n';  // expected output: 1
+       << '\n';  // expected output: 2
+  cout << "s.trailingZeroes(30) -> " << s.trailingZeroes(30)
+       << '\n';  // expected output: 7
 
   return 0;
 }
