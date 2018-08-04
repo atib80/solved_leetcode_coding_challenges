6210d9366251f854aaf27bcef993b858b51e8ca3
Atila Budai <atib1980@gmail.com>
2018-08-04 22:20:12 +0200

    initial recursive solution

--- 3_hard_difficulty_level/97_interleaving_string.cpp
+++ 3_hard_difficulty_level/97_interleaving_string.cpp
@@ -13,6 +13,8 @@ Example 2:
 Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
 Output: false
 */
+
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -25,28 +27,46 @@ class Solution {
                                       const string& s2,
                                       const int s2_len,
                                       const string& s3,
-                                      const size_t s3_len,
+                                      const int s3_len,
                                       const int s1_pos,
                                       const int s2_pos,
-                                      const size_t s1_next_s3_pos,
-                                      const size_t s2_next_s3_pos,
+                                      const int s1_next_s3_pos,
+                                      const int s2_next_s3_pos,
                                       vector<int>& visited_s3_chars) {
-    if (s1_pos == s1_len && s1_next_s3_pos > s3_len - s2_len ||
-        s2_pos == s2_len && s2_next_s3_pos < s3_len - s1_len)
+    if (s1_pos == s1_len && s2_pos == -1)
       return true;
 
-    while (s1_pos < s1_len || s2_pos >= 0) {
-      for (size_t i{s1_next_s3_pos}; i < s3_len - s1_len; i++) {
-        const size_t next_s1_pos{s3.find(s1[s1_pos], i)};
-        if (string::npos != next_s1_pos) {
-          if (!visited_s3_chars[next_s1_pos]) {
-            visited_s3_chars[next_s1_pos] = 1;
-            if (check_if_s1_and_s2_interleaved(
-                    s1, s1_len, s2, s2_len, s3, s3_len, s1_pos + 1, s2_pos,
-                    next_s1_pos + 1, s2_next_s3_pos, visited_s3_chars))
-              return true;
-            visited_s3_chars[next_s1_pos] = 0;
+    if (s1_pos < s1_len || s2_pos >= 0) {
+      int i{s1_next_s3_pos}, j{s2_next_s3_pos};
+      while (i < s3_len || j >= 0) {
+        if (i < s3_len) {
+          const size_t next_s1_pos{s3.find(s1[s1_pos], i)};
+          if (string::npos != next_s1_pos) {
+            if (!visited_s3_chars[next_s1_pos]) {
+              visited_s3_chars[next_s1_pos] = 1;
+              if (check_if_s1_and_s2_interleaved(
+                      s1, s1_len, s2, s2_len, s3, s3_len, s1_pos + 1, s2_pos,
+                      next_s1_pos + 1, j, visited_s3_chars))
+                return true;
+              visited_s3_chars[next_s1_pos] = 0;
+            }
           }
+          i++;
+        }
+
+        if (j >= 0) {
+          const size_t next_s2_pos{s3.rfind(s2[s2_pos], j)};
+          if (string::npos != next_s2_pos) {
+            if (!visited_s3_chars[next_s2_pos]) {
+              visited_s3_chars[next_s2_pos] = 1;
+              if (check_if_s1_and_s2_interleaved(
+                      s1, s1_len, s2, s2_len, s3, s3_len, s1_pos, s2_pos - 1, i,
+                      next_s2_pos - 1, visited_s3_chars))
+                return true;
+              visited_s3_chars[next_s2_pos] = 0;
+            }
+          }
+          j--;
         }
       }
     }
@@ -61,11 +81,14 @@ class Solution {
     const size_t s1_len{s1.length()};
     const size_t s2_len{s2.length()};
     const size_t s3_len{s3.length()};
-    if (s1_len + s2_len != s3_len)
+    if (!s1_len && !s2_len && !s3_len)
+      return true;
+    if ((s1_len + s2_len != s3_len) || (!s1_len && !s2_len))
       return false;
+    if ((!s1_len && s2 == s3) || (!s2_len && s1 == s3))
+      return true;
     const size_t min_s1_s2_len{s1_len <= s2_len ? s1_len : s2_len};
     const size_t max_s1_s2_len{s1_len > s2_len ? s1_len : s2_len};
-    // const string& shorter_str{s1_len <= s2_len ? s1 : s2};
     const string& longer_str{s1_len > s2_len ? s1 : s2};
 
     for (size_t i{}; i < min_s1_s2_len; i++) {
@@ -79,19 +102,39 @@ class Solution {
     for (size_t i{}; i < s3_len; i++)
       char_freq_s3[s3[i]]++;
 
-    for (size_t i{}; i < 128; i++) {
+    for (size_t i{}; i < 128; i++)
       if (char_freq_s3[i] && char_freq_s3[i] != char_freq_s1_s2[i])
         return false;
-    }
 
     vector<int> visited_s3_chars(s3_len, 0);
 
-    return true;
+    if (check_if_s1_and_s2_interleaved(s1, s1_len, s2, s2_len, s3, s3_len, 0,
+                                       s2_len - 1, 0, s3_len - 1,
+                                       visited_s3_chars))
+      return true;
+
+    visited_s3_chars.clear();
+
+    if (check_if_s1_and_s2_interleaved(s2, s2_len, s1, s1_len, s3, s3_len, 0,
+                                       s1_len - 1, 0, s3_len - 1,
+                                       visited_s3_chars))
+      return true;
+
+    return false;
   }
 };
 
 int main() {
   Solution s{};
+
+  cout << boolalpha
+       << "s.isInterleave(\"aabcc\", \"dbbca\", \"aadbbcbcac\") -> "
+       << s.isInterleave("aabcc", "dbbca", "aadbbcbcac")
+       << '\n'  // expected output: true
+       << "s.isInterleave(\"aabcc\", \"dbbca\", \"aadbbbaccc\") -> "
+       << s.isInterleave("aabcc", "dbbca", "aadbbbaccc")
+       << '\n'  // expected output: false
+       << noboolalpha;
 
   return 0;
 }
