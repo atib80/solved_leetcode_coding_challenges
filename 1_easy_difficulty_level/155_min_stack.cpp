8fa7d523712a113b3b117b9365e1b012bcae624a
Atila Budai <atib1980@gmail.com>
2018-08-23 18:56:16 +0200

    generic MinStack class, optimization

--- 1_easy_difficulty_level/155_min_stack.cpp
+++ 1_easy_difficulty_level/155_min_stack.cpp
@@ -21,6 +21,7 @@ minStack.top();      --> Returns 0.
 minStack.getMin();   --> Returns -2.
 */
 
+#include <functional>
 #include <iostream>
 #include <stdexcept>
 #include <utility>
@@ -28,21 +29,29 @@ minStack.getMin();   --> Returns -2.
 
 using namespace std;
 
+template <typename T, typename MinOperation = less<T>>
 class MinStack {
   size_t min_element_index;
-  vector<pair<int, size_t>> s;
+  vector<pair<T, size_t>> s;
+  MinOperation min_op_;
 
  public:
   MinStack() : min_element_index{} { s.reserve(1024); }
 
-  int getMin() {
+  template <typename MinOperationFunc>
+  MinStack(MinOperationFunc&& min_op)
+      : min_element_index{}, min_op_{forward<MinOperationFunc>(min_op)} {
+    s.reserve(1024);
+  }
+
+  T getMin() {
     if (s.empty())
       throw runtime_error{"Stack is empty!"};
 
     return s[min_element_index].first;
   }
 
-  int top() {
+  T top() {
     if (s.empty())
       throw runtime_error{"Stack is empty!"};
 
@@ -53,7 +62,6 @@ class MinStack {
     if (s.empty())
       throw runtime_error{"Stack is empty!"};
 
-    const int t{s.back().first};
     s.pop_back();
     if (!s.empty())
       min_element_index = s.back().second;
@@ -68,23 +76,31 @@ class MinStack {
 
     s.emplace_back(make_pair(x, min_element_index));
 
-    if (x < s[min_element_index].first)
+    if (min_op_(x, s[min_element_index].first))
       min_element_index = s.back().second = s.size() - 1;
-    else
-      s.back().second = min_element_index;
   }
 };
 
 int main() {
-  MinStack ms{};
+  MinStack<int> ms{};
 
-  ms.push(-2);
-  ms.push(0);
-  ms.push(-3);
-  ms.getMin();
-  ms.pop();
-  ms.top();
-  ms.getMin();
+  try {
+    ms.push(-2);
+    printf("1. ms.push(-2);\n");
+    ms.push(0);
+    printf("2. ms.push(0);\n");
+    ms.push(-3);
+    printf("3. ms.push(-3);\n");
+    cout << "4. ms.getMin() -> " << ms.getMin() << '\n';  // expected output: -3
+    ms.pop();
+    printf("5. ms.pop();\n");
+    cout << "6. ms.top() -> " << ms.top() << '\n';        // expected output: 0
+    cout << "7. ms.getMin() -> " << ms.getMin() << '\n';  // expected output: -2
+
+  } catch (exception& e) {
+    cerr << e.what() << '\n';
+    return 1;
+  }
 
   return 0;
 }
